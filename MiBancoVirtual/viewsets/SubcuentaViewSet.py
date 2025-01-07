36edbd3b359f28579ff0169f1aa731c4d01a0f86
Fabricio Mejía <fabricio.mejia@cooitza.com.gt>
2025-01-06 20:00:34 -0600

    Fix: orden de subcuentas

--- MiBancoVirtual/viewsets/SubcuentaViewSet.py
+++ MiBancoVirtual/viewsets/SubcuentaViewSet.py
@@ -51,30 +51,4 @@ class SubcuentaViewSet(viewsets.ModelViewSet):
                 Value(" - "),
                 F("perfil__nombre")
             ) 
-        ).order_by("-saldo")
-        return Subcuenta.objects.raw("""
-            SELECT 
-                subcuenta.id,
-                IFNULL(
-                    (
-                        SELECT 
-                            SUM(	
-                                CASE
-                                    WHEN transaccion.ordenante_id = subcuenta.id THEN - transaccion.monto 
-                                    WHEN transaccion.beneficiario_id = subcuenta.id THEN transaccion.monto 
-                                END
-                            ) as Saldo 
-                        FROM MiBancoVirtual_transaccion transaccion
-                    ), 0
-                ) AS saldo,
-                (cuenta.nombre || " - " || perfil.nombre) as subcuenta_nombre,
-                subcuenta.cuenta_id as idCuenta,
-                cuenta.nombre as cuenta_nombre,
-                subcuenta.perfil_id as idPerfil,
-                perfil.nombre as perfil_nombre
-            FROM MiBancoVirtual_subcuenta subcuenta
-            INNER JOIN MiBancoVirtual_cuenta cuenta on cuenta.id = subcuenta.cuenta_id 
-            INNER JOIN auth_user usuario on usuario.id = cuenta.usuario_id 
-            INNER JOIN MiBancoVirtual_perfil perfil on perfil.id = subcuenta.perfil_id 
-            WHERE usuario.id = %s   
-        """, [idUsuario])
+        ).order_by("subcuenta_nombre")
